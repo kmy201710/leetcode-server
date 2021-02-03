798210ce8ea1235aaad52370d5130b580dbb2631
fastmdr@163.com <fastmdr@163.com>
2021-02-03 21:01:42 +0800

    leetcode

--- src/main/java/com/leetcode/Solution002.java
+++ src/main/java/com/leetcode/Solution002.java
@@ -4,8 +4,6 @@ import com.leetcode.model.ListNode;
 import com.leetcode.utils.RandomUtils;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -15,16 +13,11 @@ import java.util.Scanner;
  * 2. 两数相加
  * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
  *
- * 请你将两个数相加，并以相同形式返回一个表示和的链表。
- *
- * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
- *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/add-two-numbers
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution002 {
-    private static Map<Integer, Integer> map = new HashMap();
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = null, tail = null;
@@ -58,8 +51,8 @@ public class Solution002 {
     public static void main(String[] args) {
         // 接收从控制台输入
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt() % 10;
-        n = n < 5 ? 5 : n;
+        int n = input.nextInt();
+        n = n % 10 < 5 ? 5 : n % 10;
         // 获取随机数
         int[] nums = RandomUtils.randomCommon(0, 10, n);
         System.out.println("nums = " + Arrays.toString(nums));
